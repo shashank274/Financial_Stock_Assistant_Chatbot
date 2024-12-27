9775f50b27d67ee6076520f1aab6dd9c90ab73f6
shashank274 <shashank.kanangi@hotmail.com>
2024-12-28 00:29:35 +0400

    commit 3

--- model.py
+++ model.py
@@ -24,4 +24,29 @@ def calculate_RSI(ticker):
     
     up = delta.clip(lower = 0)
     down = -1 * delta.clip(upper=0)
-    ema_UP = 
+    ema_up = up.ewm(com=14-1, adjust=False).mean()
+    ema_down = down.ewm(com=14-1, adjust=False).mean()
+    rs = ema_up/ema_down
+    return str(100 - (100/(1+rs)).iloc[-1])
+
+def calculate_MACD(ticker):
+    data = yf.Ticker(ticker).history(period='1y').Close
+    short_EMA = data.ewm(span = 12, adjust=False).mean()
+    long_EMA = data.ewm(span = 26, adjust=False).mean()
+    
+    MACD = short_EMA - long_EMA
+    signal = MACD.ewm(span=9, adjust=False).mean()
+    MACD_histogram = MACD - signal
+    
+    return f'{MACD[-1]}, {signal[-1]}, {MACD_histogram[-1]}'
+
+def plot_stock_price(ticker):
+    data = yf.Ticker(ticker).history(period='1y').Close
+    plt.figure(figsize=(10,5))
+    plt.plot(data.index,data.Close)
+    plt.title('{Ticker} Stock Price Over Last Year')
+    plt.xlabel('Date')
+    plt.ylabel('Stock Price ($)')
+    plt.grid=True
+    plt.savefig('stock.png')
+    plt.close()
