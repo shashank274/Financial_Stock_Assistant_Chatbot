e19eddad3c539648b6beac75866eaec3841b5255
shashank274 <shashank.kanangi@hotmail.com>
2024-12-27 17:26:39 +0400

    commit 2

--- model.py
+++ model.py
@@ -7,4 +7,21 @@ import yfinance as yf
 
 open.api_key = open('API_KEY','r').read()
 
-def get_stock_price()
+def get_stock_price(ticker):
+    return str(yf.Ticker(ticker).history(period='1y').iloc[-1].Close)
+
+def calculate_SMA(ticker,window):
+    data = yf.Ticker(ticker).history(period='1y').Close
+    return str(data.rolling(window=window).mean().iloc[-1])
+
+def calculate_EMA(ticker,window):
+    data = yf.Ticker(ticker).history(period='1y').Close
+    return str(data.ewm(span=window, adjust = False).mean().iloc[-1])
+
+def calculate_RSI(ticker):
+    data = yf.Ticker(ticker).history(period='1y').Close
+    delta = data.diff()
+    
+    up = delta.clip(lower = 0)
+    down = -1 * delta.clip(upper=0)
+    ema_UP = 
