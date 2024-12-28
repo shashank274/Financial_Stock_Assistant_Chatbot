b70b497e2287a801a73763efbf16ad0f7c1f2754
shashank274 <shashank.kanangi@hotmail.com>
2024-12-28 15:52:18 +0400

    commit 6

--- model.py
+++ model.py
@@ -139,11 +139,61 @@ functions = [
 
 available_functions = {
     'get_stock_price' : get_stock_price,
-    'calculate_SMA' : calculate_SMA
-    'calculate_EMA' : calculate_EMA
-    'calculate_RSI' : calculate_RSI
-    'calculate_MACD' : calculate_MACD
+    'calculate_SMA' : calculate_SMA,
+    'calculate_EMA' : calculate_EMA,
+    'calculate_RSI' : calculate_RSI,
+    'calculate_MACD' : calculate_MACD,
     'plot_stock_price' : plot_stock_price
     
 }
 
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+    
+st.title('Stock Analysis Chatbot Assistant')
+
+user_input = st.text_input('Your Input :')
+
+if user_input:
+    try:
+        st.session_state['messages'].append({'role': 'user','content' : f'{user_input}'})
+        
+        response = openai.ChatCompletion.create(
+            model = 'gpt-3.5=turbo-0613',
+            messages = st.session_state['messages'],
+            functions = functions,
+            function_call = 'auto'
+        )
+        response_message = response['choices'][0]['message']
+        
+        if response_message.get('function_call'):
+            function_name = response_message['function_call']['name']
+            function_args = json.loads(response_message['function_call']['arguments'])
+            
+            if function_name in ['get_stock_price', 'calculate_RSI', 'calculate_MACD', 'plot_stock_price']:
+                args_dict = {'ticker': function_args.get('ticker')}
+                
+            elif function_name in ['calculate_SMA', ' calculate_EMA']:
+                args_dict = {'ticker': function_args.get('ticker'), 'window' : function_args.get('window')}
+                
+            function_to_call = available_functions[function_name]
+            function_response = function_to_call(**args_dict)
+            
+            if function_name == 'plot_stock_price':
+                st.image('stock.png')
+            else:
+                st.session_state['messages'].append(response_message)
+                st.session_state['messages'].append(
+                    {
+                        'role' : 'function',
+                        'name' : function_name,
+                        'content' : function_response
+                    }
+                )
+                second_response = openai.ChatCompletion.create(
+                    model = 'gpt-3.5=turbo-0613',
+                    messages = st.session_state['messages']
+                )
+                st.text(second_response['messages'].append('role' : 'assistant', 'content' : response_message['content']))
+                
+    except:
